b5aa5cfc35625be0f2b39d8f26f9f657796bff5b
Barbara1111-hub <barbara.soraya.sl@gmail.com>
2024-07-18 16:04:14 -0400

    Create 05like.sql

---
+++ 05like.sql
@@ -0,0 +1,2 @@
+SELECT * FROM users WHERE email = 'barbara.soraya.sl@gmail.com'; // entrega especificamente los usuarios que tengan ese correo
+SELECT * FROM users WHERE email LIKE '%gmail.com'; // este codigo entrega los datos de los usuarios que tengan correo gmail, se utiliza % para indicar que no importa lo que contenga antes de gmail
