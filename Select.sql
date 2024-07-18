76029be93244ae812542b058cfd267640ce0cc8c
Barbara1111-hub <barbara.soraya.sl@gmail.com>
2024-07-18 15:25:17 -0400

    Select.sql

---
+++ Select.sql
@@ -0,0 +1,4 @@
+SELECT name FROM users // muestra todos los nombres de los usuarios
+SELECT * FROM users // muestra toda la tabla de usuarios
+SELECT user_id, name FROM users // muestra el id y el nombre de la tabla de usuarios
+
