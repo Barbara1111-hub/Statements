e492defcf9bd5855efbd158d8313b7c4e2c8c2cd
Barbara1111-hub <barbara.soraya.sl@gmail.com>
2024-07-18 16:22:55 -0400

    Update 01Select.sql

--- 01Select.sql
+++ 01Select.sql
@@ -1,4 +1,5 @@
-SELECT name FROM users // muestra todos los nombres de los usuarios
-SELECT * FROM users // muestra toda la tabla de usuarios
-SELECT user_id, name FROM users // muestra el id y el nombre de la tabla de usuarios
+SELECT name FROM users /* muestra todos los nombres de los usuarios */
+SELECT * FROM users /* muestra toda la tabla de usuarios */
+SELECT user_id, name FROM users /* muestra el id y el nombre de la tabla de usuarios */
+
 
