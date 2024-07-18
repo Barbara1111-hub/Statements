bda3978aa8f7c05667c82ec76a467ebb6789ce6e
Barbara1111-hub <barbara.soraya.sl@gmail.com>
2024-07-18 16:22:20 -0400

    Create 07Limit.sql

---
+++ 07Limit.sql
@@ -0,0 +1,4 @@
+SELECT * FROM users LIMIT 2 /* entrega los dos primeros datos de la tabla */
+SELECT * FROM users WHERE age <30 or email like '%soraya%' LIMIT 2 /* entrega los dos primeros resultados de la consulta anterior
+/* se utiliza cuando las bases de datos son muy grandes, para limitar las consultas
+/*Comentarios
